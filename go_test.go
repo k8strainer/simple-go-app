fc4f134fcb8fc44d33743592d0b1c18ab42fdbd7
k8strainer <65672201+k8strainer@users.noreply.github.com>
2024-11-06 21:55:38 +0100

    Create go_test.go

---
+++ go_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func TestHelloHandler(t *testing.T) {
+    req, err := http.NewRequest("GET", "/", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(helloHandler)
+    handler.ServeHTTP(rr, req)
+
+    assert.Equal(t, http.StatusOK, rr.Code)
+    assert.Equal(t, "Hello, World!\n", rr.Body.String())
+}
